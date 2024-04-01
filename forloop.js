25cdd4da38beecc7515d24a2d942578a1127e4f6
Gyanendra <gyanen56@gmail.com>
2024-04-01 13:15:19 +1100

    while loop

--- forloop.js
+++ forloop.js
@@ -5,3 +5,5 @@ for(let i=0; i<names.length; i++){
 }
 // output Hari Sam Joe
 
+
+
---
+++ whileloop.js
@@ -0,0 +1,8 @@
+const names = ['Hari', 'Sam', 'Joe']
+
+let i=0;
+while(i<names.length){
+    console.log(names[i]);
+    i++
+}
+// output : Hari Sam Joe
