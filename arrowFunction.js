ebaa154d387bcf79d53e24928f6c673c27be43e3
Gyanendra <gyanen56@gmail.com>
2024-04-05 13:43:31 +1100

    arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,8 @@
+const withDefaultValue = (name, sureName="white")=>{
+    console.log(name, sureName)
+}
+
+withDefaultValue("gyan" ,"poudel");
+// gyan poudel
+withDefaultValue('gyan')
+// gyan white
