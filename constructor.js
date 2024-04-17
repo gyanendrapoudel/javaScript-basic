9cdbd63b306253505d2005e15ad711bdd06b813d
Gyanendra <gyanen56@gmail.com>
2024-04-17 20:51:46 +1000

    constructor

---
+++ constructor.js
@@ -0,0 +1,26 @@
+function AboutPerson(name,add){
+    this.name=name
+    this.add=add
+}
+
+
+const gyan =  new AboutPerson('Gyanendra','Sydney')
+console.log(gyan)
+
+/* AboutPerson {
+    "name": "Gyanendra",
+    "add": "Sydney"
+}
+*/
+const david =  new AboutPerson('David', 'Melbourne')
+
+AboutPerson.prototype.bio = function(){
+    return `Hey, this is ${this.name}. I live in ${this.add}`
+}
+console.log(david)
+
+console.log(gyan.bio())
+// Hey, this is Gyanendra. I live in Sydney
+console.log(david.bio())
+// Hey, this is David. I live in Melbourne
+
