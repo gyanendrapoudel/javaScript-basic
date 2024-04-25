0e2a499a31533b7984ddfcdc16345bb71885c7c5
Gyanendra <gyanen56@gmail.com>
2024-04-25 16:08:50 +1000

    after abstraction

--- abstraction1.js
+++ abstraction1.js
@@ -1,15 +1,15 @@
 class Person{
-
+ #dob
     constructor(obj){
         this.name = obj.name,
         this.add= obj.add
-        this.dob = obj.dob
+        this.#dob = obj.dob
     }
     bio(){
         return `This is ${this.name} from ${this.add} and I am ${this.getAge()} years old. `
     }
     getAge(){
-        return new Date().getFullYear()-new Date(this.dob).getFullYear()
+        return new Date().getFullYear()-new Date(this.#dob).getFullYear()
     }
 }
 const p1 = new Person({name:'gyanendra', dob:"1992-12-11"})
