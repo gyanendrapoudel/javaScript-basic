1f5caa8551a7747d283b700bb256114372d3e839
Gyanendra <gyanen56@gmail.com>
2024-04-25 16:13:52 +1000

    abstraction function and variable

--- abstraction1.js
+++ abstraction1.js
@@ -6,12 +6,20 @@ class Person{
         this.#dob = obj.dob
     }
     bio(){
-        return `This is ${this.name} from ${this.add} and I am ${this.getAge()} years old. `
+        return `This is ${this.name} from ${this.add} and I am ${this.#getAge()} years old. `
     }
-    getAge(){
+    #getAge(){
         return new Date().getFullYear()-new Date(this.#dob).getFullYear()
     }
 }
-const p1 = new Person({name:'gyanendra', dob:"1992-12-11"})
-console.log(p1.dob) //1992-12-11
-console.log(p1.getAge()) //32
+const p1 = new Person({name:'gyanendra', add:'Sydney', dob:"1992-12-11"})
+// console.log(p1.dob) //1992-12-11
+// console.log(p1.getAge()) //32
+
+// console.log(p1.dob) //undefined
+// console.log(p1.getAge()) //32
+
+console.log(p1.dob) //undefined
+
+// console.log(p1.getAge()) //Error: p1.getAge is not a function
+console.log(p1.bio()) // This is gyanendra from Sydney and I am 32 years old. 
